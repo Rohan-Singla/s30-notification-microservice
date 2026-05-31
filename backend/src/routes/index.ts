57e7e98dfaa1d1cd407fe94537054f2180e11a86
Rohan Singla <rohansinglawork@gmail.com>
2026-05-31 23:14:54 +0530

    setup

---
+++ backend/src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import { login, signup } from "../controllers/authcontroller";
+import { marketingEmail } from "../controllers/marketingcontroller";
+import { walletOnramp } from "../controllers/walletcontroller";
+import { adminMiddleware, authMiddleware } from "../middleware/auth";
+
+export const router = Router();
+
+router.post("/auth/signup", signup);
+
+router.post("/auth/login", login);
+router.post("/wallet/onramp", authMiddleware, walletOnramp);
+router.post("/email/marketing", authMiddleware, adminMiddleware, marketingEmail);
+router.get("/health", (_req, res) => {
+  res.json({
+    message: "OK",
+    service: "backend",
+  });
+});
